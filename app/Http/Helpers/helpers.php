0a6d79079042b255c2e019a642a09cdac184336a
dwiyuliawan <dwiyuliawan25@gmail.com>
2025-01-07 14:20:37 +0700

    crate helpers

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -13,19 +13,50 @@
 		if ($angka < 12) { // 1 -11
 			$terbilang = '' .$baca[$angka];
 		}elseif ($angka < 20) { // 12 - 19
-			$terbilang = terbilang($angka - 10). 'belas';
+			$terbilang = terbilang($angka - 10).'belas';
 		}elseif ($angka < 100) { // 20 - 99
-			$terbilang = terbilang($angka / 10). 'puluh'. ' '. terbilang($angka % 10); //fungsi modulus
+			$terbilang = terbilang($angka / 10).'puluh'. terbilang($angka % 10); //fungsi modulus
 		}elseif ($angka < 200) { // 100 - 199
-			$terbilang = 'seratus'.' '. terbilang($angka - 100);
+			$terbilang = 'seratus'. terbilang($angka - 100);
 		}elseif ($angka <  1000) { // 200 - 999
-			$terbilang = terbilang($angka / 100). 'ratus'. ' '.terbilang($angka % 100);
+			$terbilang = terbilang($angka / 100).'ratus'.terbilang($angka % 100);
 		}elseif ($angka < 2000) { // 1000 - 1999
 			$terbilang = 'seribu'.' '.terbilang($angka - 1000);
-		}elseif (condition) {
-			// code...
+		}elseif ($angka < 1000000) { //2000 - 999.999
+			$terbilang = terbilang($angka / 1000).'ribu'. terbilang($angka % 1000);
+		}elseif($angka < 1000000000){
+			$terbilang = terbilang($angka / 1000000).'juta'. terbilang($angka % 1000000);
 		}
 
 		return $terbilang;
+	}
+
+	//membuat function tanggal dan hari indonesia
+	function tanggal_indonesia($tgl, $tampil_hari = true)
+	{
+
+		$nama_hari = array(
+			'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jum\'at', 'Sabtu'
+		);
+
+		$nama_bulan = array(1 => 
+			'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+		);
+
+		//2025-01-07
+		$tahun = substr($tgl, 0, 4);
+		$bulan = $nama_bulan[(int) substr($tgl, 5, 2)];
+		$tanggal = substr($tgl, 8, 2);
+		$text = '';
+
+		$urutan_hari = date('w', mktime(0,0,0, substr($tgl, 5,2), $tanggal, $tahun));
+		$hari = $nama_hari[$urutan_hari];
+
+		if ($tampil_hari) {
+		$text.= "$hari, $tanggal $bulan $tahun";
+		}else{
+		$text.= "$tanggal $bulan $tahun";
+		}
+		return $text;
 	}
 ?>
